d859a6a888564138a8e96a74aabbfabd054de957
Juno <junonguyen27@gmail.com>
2021-12-22 21:56:03 -0600

    taking a break for today, left to do is saving using local storage and make save feature work

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,14 +5,28 @@ function currentDay() {
     $("#currentDay").text(currentTime.format("dddd, MMMM Do"));
 };
 
-var saveButton = document.querySelector(".saveBtn");
+// var saveButton = document.querySelector(".saveBtn");
 
-saveButton.addEventListener('click', changeBackground);
+// saveButton.addEventListener('click', changeBackground);
 
- var tester = saveButton.previousElementSibling;
+//  var tester = saveButton.previousElementSibling;
 
- function changeBackground() {
-     tester.style.backgroundColor = "green";
- }
+//  function changeBackground() {
+//      tester.style.backgroundColor = "green";
+//  }
 
+// var saveButton = document.querySelector(".saveBtn");
+// var calenderBG = document.querySelector(".calender");
+
+// var mode = "default";
+
+// saveButton.addEventListener('click', function() {
+//     if (mode === "default") {
+//         mode = "saved";
+//         calenderBG.setAttribute("class","saved");
+//     } else {
+//         mode = "default";
+//         calenderBG.setAttribute("class", "default");
+//     }
+// });
  
