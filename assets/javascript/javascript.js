d695e968eba67134a52cd805d89301b0fbde0943
Juno <junonguyen27@gmail.com>
2021-12-23 20:25:36 -0600

    finished localstorage saving and loading data when page is refreshed

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,96 +5,113 @@ function currentDay() {
     $("#currentDay").text(currentTime.format("dddd, MMMM Do"));
 };
 
+var calender1 = document.querySelector("#calender1");
+var calender2 = document.querySelector("#calender2");
+var calender3 = document.querySelector("#calender3");
+var calender4 = document.querySelector("#calender4");
+var calender5 = document.querySelector("#calender5");
+var calender6 = document.querySelector("#calender6");
+var calender7 = document.querySelector("#calender7");
+var calender8 = document.querySelector("#calender8");
+var calender9 = document.querySelector("#calender9");
+
 
 // all calender save functions
 
 function changeBackground1() {
-    var element = document.querySelector("#calender1");
-    element.style.backgroundColor = "var(--saved)";
-
-    var calenderOneTxt = document.querySelector("#calender1").value;
-    localStorage.setItem("Calender1", calenderOneTxt);
-    alert("Calender1 Saved!");
-    console.log("Calender1 Saved!");
+    calender1.style.backgroundColor = "var(--saved)";
+    localStorage.setItem("Calender1", calender1.value);
 };
 
 function changeBackground2() {
-    var element = document.querySelector("#calender2");
-    element.style.backgroundColor = "var(--saved)";
-
-    var calenderTwoTxt = document.querySelector("#calender2").value;
-    localStorage.setItem("Calender2", calenderTwoTxt);
-    alert("Calender2 Saved!");
-    console.log("Calender2 Saved!");
+    calender2.style.backgroundColor = "var(--saved)";
+    localStorage.setItem("Calender2", calender2.value);
 };
 
 function changeBackground3() {
-    var element = document.querySelector("#calender3");
-    element.style.backgroundColor = "var(--saved)";
-
-    var calenderThreeTxt = document.querySelector("#calender3").value;
-    localStorage.setItem("Calender3", calenderThreeTxt);
-    alert("Calender3 Saved!");
-    console.log("Calender3 Saved!");
+    calender3.style.backgroundColor = "var(--saved)";
+    localStorage.setItem("Calender3", calender3.value);
 };
 
 function changeBackground4() {
-    var element = document.querySelector("#calender4");
-    element.style.backgroundColor = "var(--saved)";
-
-    var calenderFourTxt = document.querySelector("#calender4").value;
-    localStorage.setItem("Calender4", calenderFourTxt);
-    alert("Calender4 Saved!");
-    console.log("Calender4 Saved!");
+    calender4.style.backgroundColor = "var(--saved)";
+    localStorage.setItem("Calender4", calender4.value);
 };
 
 function changeBackground5() {
-    var element = document.querySelector("#calender5");
-    element.style.backgroundColor = "var(--saved)";
-
-    var calenderFiveTxt = document.querySelector("#calender5").value;
-    localStorage.setItem("Calender5", calenderFiveTxt);
-    alert("Calender5 Saved!");
-    console.log("Calender5 Saved!");
+    calender5.style.backgroundColor = "var(--saved)";
+    localStorage.setItem("Calender5", calender5.value);
 };
 
 function changeBackground6() {
-    var element = document.querySelector("#calender6");
-    element.style.backgroundColor = "var(--saved)";
-
-    var calenderSixTxt = document.querySelector("#calender6").value;
-    localStorage.setItem("Calender6", calenderSixTxt);
-    alert("Calender6 Saved!");
-    console.log("Calender6 Saved!");
+    calender6.style.backgroundColor = "var(--saved)";
+    localStorage.setItem("Calender6", calender6.value);
 };
 
 function changeBackground7() {
-    var element = document.querySelector("#calender7");
-    element.style.backgroundColor = "var(--saved)";
-
-    var calenderSevenTxt = document.querySelector("#calender7").value;
-    localStorage.setItem("Calender7", calenderSevenTxt);
-    alert("Calender7 Saved!");
-    console.log("Calender7 Saved!");
+    calender7.style.backgroundColor = "var(--saved)";
+    localStorage.setItem("Calender7", calender7.value);
 };
 
 function changeBackground8() {
-    var element = document.querySelector("#calender8");
-    element.style.backgroundColor = "var(--saved)";
-
-    var calenderEightTxt = document.querySelector("#calender8").value;
-    localStorage.setItem("Calender8", calenderEightTxt);
-    alert("Calender8 Saved!");
-    console.log("Calender8 Saved!");
+    calender8.style.backgroundColor = "var(--saved)";
+    localStorage.setItem("Calender8", calender8.value);
 };
 
 function changeBackground9() {
-    var element = document.querySelector("#calender9");
-    element.style.backgroundColor = "var(--saved)";
-
-    var calenderNineTxt = document.querySelector("#calender9").value;
-    localStorage.setItem("Calender9", calenderNineTxt);
-    alert("Calender9 Saved!");
-    console.log("Calender9 Saved!");
+    calender9.style.backgroundColor = "var(--saved)";
+    localStorage.setItem("Calender9", calender9.value);
 };
 // END save functions
+
+// make calender background color change when unfocused
+
+function backgroundRed1() {
+    calender1.style.backgroundColor = "var(--unsaved)";
+};
+
+function backgroundRed2() {
+    calender2.style.backgroundColor = "var(--unsaved)";
+};
+
+function backgroundRed3() {
+    calender3.style.backgroundColor = "var(--unsaved)";
+};
+
+function backgroundRed4() {
+    calender4.style.backgroundColor = "var(--unsaved)";
+};
+
+function backgroundRed5() {
+    calender5.style.backgroundColor = "var(--unsaved)";
+};
+
+function backgroundRed6() {
+    calender6.style.backgroundColor = "var(--unsaved)";
+};
+
+function backgroundRed7() {
+    calender7.style.backgroundColor = "var(--unsaved)";
+};
+
+function backgroundRed8() {
+    calender8.style.backgroundColor = "var(--unsaved)";
+};
+
+function backgroundRed9() {
+    calender9.style.backgroundColor = "var(--unsaved)";
+};
+
+// END background red when unsaved
+
+// maintain textarea text content when page refreshes
+
+$(document).ready(function () {
+    for (var i = 1; i < 9; i++) {
+    $("#calender" + i).val(localStorage.getItem("Calender" + i))
+    };
+});
+
+
+
+
